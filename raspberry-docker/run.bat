26ee5c60ad524125e9492fe1d3885886705f6acd
Rich Grenwick <rgrenwick@rightpoint.com>
2017-08-28 20:58:48 -0400

    added r-base and r-base-dev to image

--- raspberry-docker/run.bat
+++ raspberry-docker/run.bat
@@ -1,4 +1,4 @@
 @ECHO OFF
 
 REM docker run -t -i -p 80:80 -p 3306:3306 -p 22:22 raspberry-docker /bin/bash
-docker run --interactive -t -i -p 80:80 -p 3306:3306 -p 22:22 rgrenwick/raspberry-docker.2  /bin/bash
+docker run --interactive -t -i -p 80:80 -p 3306:3306 -p 22:22 rgrenwick/raspberry-docker  /bin/bash
